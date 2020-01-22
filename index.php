a6d588ec83c6b9fbc3c71e1b38281e6355ca0d51
KhornSokheng <59908402+KhornSokheng@users.noreply.github.com>
2020-01-22 14:31:07 +0700

    v.5
    v.5

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
 <html>
 <body>
  
-<h1>Team v.4</h1>
+<h1>Team v.5</h1>
  
 <?php
 echo "Hello supakorn <br>";
 echo "Hello nattapong naaa <br>";
 echo "Hello janejira kub <br>";
 echo "Hello Sokheng <br>";
+echo "Hello Sokheng <dr>";
 ?>
  
 </body>
